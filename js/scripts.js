21cb8d578d2267e4205984828a75842644433349
xa <xiaesmeamen@me.com>
2017-01-24 14:15:38 -0800

    fix indentation on all documents

--- js/scripts.js
+++ js/scripts.js
@@ -44,21 +44,19 @@ $(document).ready(function(){
     } else if (popular === "no"){
       userResponse -= 2;
     };
-
-
-      if (userResponse <= 0){
-        $(".results").append("Learn more about PHP <a href='https://www.epicodus.com/php'>here!</a> ");
-        $("#php, #reset-btn").show();
-        $("#e-quiz, #submit-btn").hide();
-      } else if (userResponse <= 4 && userResponse >= 0) {
-        $(".results").append("Learn more about Ruby <a href='https://www.epicodus.com/ruby'>here!</a>!");
-        $("#ruby, #reset-btn").show();
-        $("#e-quiz, #submit-btn").hide();
-      } else if (userResponse >= 5) {
-        $(".results").append("Learn more about C# <a href=https://www.epicodus.com/c-sharp'>here!</a>!");
-        $("#c, #reset-btn").show();
-        $("#e-quiz, #submit-btn").hide();
-      };
-
+    // Results are calculated here
+    if (userResponse <= 0){
+      $(".results").append("Learn more about PHP <a href='https://www.epicodus.com/php'>here!</a> ");
+      $("#php, #reset-btn").show();
+      $("#e-quiz, #submit-btn").hide();
+    } else if (userResponse <= 4 && userResponse >= 0) {
+      $(".results").append("Learn more about Ruby <a href='https://www.epicodus.com/ruby'>here!</a>!");
+      $("#ruby, #reset-btn").show();
+      $("#e-quiz, #submit-btn").hide();
+    } else if (userResponse >= 5) {
+      $(".results").append("Learn more about C# <a href=https://www.epicodus.com/c-sharp'>here!</a>!");
+      $("#c, #reset-btn").show();
+      $("#e-quiz, #submit-btn").hide();
+    };
   });
 });
